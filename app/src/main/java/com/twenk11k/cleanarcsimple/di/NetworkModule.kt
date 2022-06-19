c129791f79da440eb4a496db0eaaf658ab677a14
Selim Demiral <selim_demiral@outlook.com>
2022-06-19 23:06:25 +0300

    Create NetworkModule with provideOkHttpClient and provideRetrofit methods

---
+++ app/src/main/java/com/twenk11k/cleanarcsimple/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.twenk11k.cleanarcsimple.di
+
+import com.twenk11k.cleanarcsimple.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+}
