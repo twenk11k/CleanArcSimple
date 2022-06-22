737338fb4ded4acf09fee5789189c6c9a5e695c0
Selim Demiral <selim_demiral@outlook.com>
2022-06-22 22:34:09 +0300

    Make Listing model parcelable by adding parcelize annotation and extending Parcelable

--- app/src/main/java/com/twenk11k/cleanarcsimple/data/model/response/Listing.kt
+++ app/src/main/java/com/twenk11k/cleanarcsimple/data/model/response/Listing.kt
@@ -1,11 +1,14 @@
 package com.twenk11k.cleanarcsimple.data.model.response
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import com.twenk11k.cleanarcsimple.util.Constants.DATE_FORMAT_PATTERN
 import com.twenk11k.cleanarcsimple.util.Constants.PARSE_FORMAT_PATTERN
+import kotlinx.parcelize.Parcelize
 import java.text.SimpleDateFormat
 import java.util.*
 
+@Parcelize
 data class Listing(
     val name: String?,
     val uid: String?,
@@ -18,7 +21,7 @@ data class Listing(
     val imageUrls: List<String?>?,
     @SerializedName("image_urls_thumbnails")
     val imageUrlsThumbnails: List<String?>?
-) {
+) : Parcelable {
 
     fun parseCreatedAt(): String? {
         val parseFormat = SimpleDateFormat(PARSE_FORMAT_PATTERN, Locale.ENGLISH)
