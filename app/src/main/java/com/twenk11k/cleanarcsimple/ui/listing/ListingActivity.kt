a2d67c886f87b6d1deb23599c535583d123e666e
Selim Demiral <selim_demiral@outlook.com>
2022-06-22 22:05:54 +0300

    Add viewModel in ListingActivity

--- app/src/main/java/com/twenk11k/cleanarcsimple/ui/listing/ListingActivity.kt
+++ app/src/main/java/com/twenk11k/cleanarcsimple/ui/listing/ListingActivity.kt
@@ -1,6 +1,7 @@
 package com.twenk11k.cleanarcsimple.ui.listing
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.twenk11k.cleanarcsimple.R
 import com.twenk11k.cleanarcsimple.databinding.ActivityListingBinding
 import com.twenk11k.cleanarcsimple.ui.binding.DataBindingActivity
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class ListingActivity : DataBindingActivity() {
 
     private val binding: ActivityListingBinding by binding(R.layout.activity_listing)
+    private val viewModel: ListingViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
