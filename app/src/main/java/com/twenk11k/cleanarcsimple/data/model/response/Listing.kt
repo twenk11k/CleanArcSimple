3f25a1e63ea91bbfc7ace0874598553d91fcc4c0
Selim Demiral <selim_demiral@outlook.com>
2022-06-22 21:17:23 +0300

    Add parseCreatedAt method to Listing model

--- app/src/main/java/com/twenk11k/cleanarcsimple/data/model/response/Listing.kt
+++ app/src/main/java/com/twenk11k/cleanarcsimple/data/model/response/Listing.kt
@@ -1,6 +1,10 @@
 package com.twenk11k.cleanarcsimple.data.model.response
 
 import com.google.gson.annotations.SerializedName
+import com.twenk11k.cleanarcsimple.util.Constants.DATE_FORMAT_PATTERN
+import com.twenk11k.cleanarcsimple.util.Constants.PARSE_FORMAT_PATTERN
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class Listing(
     @SerializedName("created_at")
@@ -14,4 +18,16 @@ data class Listing(
     val imageUrls: List<String?>?,
     @SerializedName("image_urls_thumbnails")
     val imageUrlsThumbnails: List<String?>?
-)
+) {
+
+    fun parseCreatedAt(): String? {
+        val parseFormat = SimpleDateFormat(PARSE_FORMAT_PATTERN, Locale.ENGLISH)
+        val dateFormat = SimpleDateFormat(DATE_FORMAT_PATTERN, Locale.ENGLISH)
+        val dateStart = dateFormat.parse(createdAt)
+        if (dateStart != null) {
+            return parseFormat.format(dateStart)
+        }
+        return null
+    }
+
+}
