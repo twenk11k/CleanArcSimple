1fa736278a0c23dc38893d984d64b00f3bc57f11
Selim Demiral <selim_demiral@outlook.com>
2022-06-22 21:13:29 +0300

    Create Constants with DATE_FORMAT_PATTERN and PARSE_FORMAT_PATTERN const

---
+++ app/src/main/java/com/twenk11k/cleanarcsimple/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.twenk11k.cleanarcsimple.util
+
+object Constants {
+    const val DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss.SSSSSS"
+    const val PARSE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss"
+}
