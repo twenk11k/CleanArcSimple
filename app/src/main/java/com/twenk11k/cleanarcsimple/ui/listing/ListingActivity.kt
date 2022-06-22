e428c15d56646be7103eb39973205d1b377a4d59
Selim Demiral <selim_demiral@outlook.com>
2022-06-22 21:09:05 +0300

    Rename MainActivity with ListingActivity and move it to ui/listing package

---
+++ app/src/main/java/com/twenk11k/cleanarcsimple/ui/listing/ListingActivity.kt
@@ -0,0 +1,12 @@
+package com.twenk11k.cleanarcsimple.ui.listing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.twenk11k.cleanarcsimple.R
+
+class ListingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listing)
+    }
+}
